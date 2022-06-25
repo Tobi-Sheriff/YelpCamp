13b4e60ae0a31e2bfa611d5f535a706148a8586e
Tobi Sheriff <s.oluwatobiloba30@gmail.com>
2022-06-26 00:35:03 +0100

    correction

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ const MongoDBStore = require("connect-mongo")(session);
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
+const mongoAdd = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+    // useFindAndModify: false
+}
 mongoose.connect(dbUrl, mongoAdd)
     .then(() => {
         console.log('CONNECTION OPEN!');
