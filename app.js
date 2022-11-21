41826a9ba52242d0f8382b26f9641d905636fd84
Tobi Sheriff <s.oluwatobiloba30@gmail.com>
2022-11-21 22:11:14 +0100

    some pq

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 const { boolean } = require('joi');
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-campgronds';
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-campgronds';
 
 const mongoAdd = {
   useNewUrlParser: true,
@@ -29,7 +29,7 @@ const mongoAdd = {
   useUnifiedTopology: true
   // useFindAndModify: false
 }
-mongoose.connect(dbUrl, mongoAdd)
+mongoose.connect('mongodb://localhost:27017/yelp-campgronds', mongoAdd)
   .then(() => {
     console.log('CONNECTION OPEN!');
   })
@@ -65,7 +65,7 @@ app.use(mongoSanitize({
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
 const store = new MongoDBStore({
-    url: dbUrl,
+    url: 'mongodb://localhost:27017/yelp-campgronds',
     secret,
     touchAfter: 24 * 60 * 60
 });
